a4b5d01f1c1eb6423758a8bbe962bace3f13da75
webertos <max.wow277@gmail.com>
2024-01-09 22:13:31 +0200

    output

--- Program.cs
+++ Program.cs
@@ -1,11 +1,11 @@
-string[] array1 = new string[5] {"Hell", "2", "word"};
+string[] array1 = new string[3] {"Hel", "2", "word"};
 string[] array2 = new string[array1.Length];
 void SecondArrayWithIF(string[] array1, string[] array2)
 {
     int count = 0;
-    for(int i = 0; i < array1.Length; i++)
+    for (int i = 0; i < array1.Length; i++)
     {
-        if(array1[i].Length <=3)
+        if(array1[i].Length <= 3)
         {
             array2[count] = array1[i];
             count++;
